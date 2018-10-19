5a65ff9657cc86d76149378d63c2fc257649ca57
Feras Albaroudi <feras.albaroudi@gmail.com>
2018-10-19 18:36:57 +1300

    cli wrapper

---
+++ README.md
@@ -0,0 +1,3 @@
+# Youtube-Downloader
+
+Python wrapper for youtube-dl
---
+++ log.py
@@ -0,0 +1,12 @@
+class YDL_Logger:
+    def debug(self, msg):
+        # print(msg)
+        pass
+
+    def warning(self, msg):
+        # print(msg)
+        pass
+
+    def error(self, msg):
+        print(msg)
+
