f7436fcfa68781e35518814f7b445776adf4f1ad
Feras Albaroudi <feras.albaroudi@gmail.com>
2018-10-21 15:52:34 +1300

    added message widget to monitor download progress

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -4,6 +4,7 @@ import youtube_dl
 from appJar import gui
 from log import YDL_Logger
 import time
+import sys
 
 CONFIG = "config.json"
 
@@ -11,9 +12,15 @@ CONFIG = "config.json"
 SAVE_ENTRY = "savepath"
 URL_LABEL = "URL(s) to download"
 URL_LABEL_TEXT = "1. Enter YouTube URL(s) to download into the box below (one URL per line)\n2. Set the directory to save the downloaded files\n3. Click download!"
-                    
 URL_ENTRY = "URL(s)"
 DL_BUTTON = "Download"
+FRAME_STACK = "FRAME_STACK"
+INPUT_FRAME = "INPUT_FRAME"
+OUTPUT_FRAME = "OUTPUT_FRAME"
+OUTPUT_MESSAGE = "OUTPUT_MESSAGE"
+PROGRESS_METER = "PROGRESS_METER"
+GUI_STDOUT_TEXT = ""
+GUI_STDOUT_SCROLL = "GUI_STDOUT_SCROLL"
 
 # These are set automatically, don't edit them
 savepath = "" # This the path that's displayed in the GUI without the output template
@@ -39,22 +46,36 @@ ydl_opts = {
 
 app = gui("Youtube Downloader", useTtk=True)
 
-def dl_finished(d):
-    status = d['status']
+# From https://stackoverflow.com/questions/3333334/stdout-to-tkinter-gui
+class IORedirector(object):
+    '''A general class for redirecting I/O to this Text widget.'''
+    def __init__(self, text, app, title):
+        self.text = text
+        self.app = app
+        self.title = title
+
+class StdoutRedirector(IORedirector):
+    '''A class for redirecting stdout to this Text widget.'''
+    def write(self, text):
+        self.text += text
+        self.app.setMessage(self.title, self.text)
+
+    def flush(self):
+        self.text = ""
+        self.app.setMessage(self.title, self.text)
+
+def progress_callback(progress):
+    status = progress['status']
+    filename = os.path.basename(progress['filename']).split(".")[0]    
+
     if status == 'finished':
-        print('Done downloading, now converting ...')
-
-def dl_downloading(d):
-    status = d['status']
-    if status == 'downloading':
-        percent = d['downloaded_bytes'] / d['total_bytes'] * 100
-        message = '%s: %.1f%%' % (d['filename'], percent)
-        print(message)
-
-def dl_error(d):
-    status = d['status']
-    if status == 'error':
-        print(d['Error downloading:'], d['filename'])
+        print('Converting "%s" ...' % filename)
+    elif status == 'downloading':
+        percent = progress['downloaded_bytes'] / progress['total_bytes'] * 100
+        message = 'Downloading "%s": %.1f%%' % (filename, percent)
+        print(message) 
+    elif status == 'error':
+        print(progress['Error download "%s"'], filename)
 
 # Writes cfg_dict to the config file
 def update_config(cfg_dict):
@@ -125,6 +146,12 @@ def dl_URLs(urls):
     videos = len(urls)
     count = 0
 
+    if videos == 0:
+        return
+
+    app.nextFrame(FRAME_STACK)
+    sys.stdout.flush()
+
     # Download each video once at a time so that we can keep track of them
     # as they complete and continue downloading the rest if there's an error
     while videos > 0:
@@ -133,6 +160,12 @@ def dl_URLs(urls):
             videos -= 1
             count += 1
         time.sleep(0.1) # Wait for a few milliseconds before checking again
+
+    # Wait until all downloads are complete before hiding the output frame
+    while running_threads > 0:
+        time.sleep(0.1)
+
+    app.prevFrame(FRAME_STACK)
         
 # Downloads all the URL's from the text area
 def onButtonPress(button):
@@ -151,13 +184,19 @@ def main():
     apply_config()
 
     app.setTtkTheme("clam")
-    # ydl_opts['progress_hooks'] = [dl_finished, dl_downloading, dl_error]
+    ydl_opts['progress_hooks'] = [progress_callback]
     
     # Window settings
     app.setPadding(10,10)
+    app.setSize(600, 300)
 
     # Adds a right-click edit menu to all entry widgets
     app.addMenuEdit()
+
+    app.startFrameStack(FRAME_STACK, start=0)
+
+    # --- Input Frame
+    app.startFrame(INPUT_FRAME)
 
     # URL entry
     app.addLabel(URL_LABEL, URL_LABEL_TEXT)
@@ -171,9 +210,31 @@ def main():
 
     # Download button
     app.addButtons([DL_BUTTON], onButtonPress)
+    app.stopFrame()
+    
+    # --- Output Frame
+    app.startFrame(OUTPUT_FRAME)
+
+    # Message box to display progress
+    app.startScrollPane(GUI_STDOUT_SCROLL)
+    app.addMessage(OUTPUT_MESSAGE, GUI_STDOUT_TEXT)
+    # app.setMessageRelief(OUTPUT_MESSAGE, 'sunken')
+    app.setMessageAlign(OUTPUT_MESSAGE, "left")
+    app.setMessageAnchor(OUTPUT_MESSAGE, "nw")
+    app.getMessageWidget(OUTPUT_MESSAGE).config(font="Courier 9")
+    app.setMessageWidth(OUTPUT_MESSAGE, 800)
+    app.stopScrollPane()
+
+    app.stopFrame()
+    app.stopFrameStack()
+
+    # Re-route stdout to our gui
+    sys.stdout = StdoutRedirector(GUI_STDOUT_TEXT, app, OUTPUT_MESSAGE)
 
     # Start GUI - nothing should happen after this
     app.go()
+
+    sys.stdout = sys.__stdout__
 
 if __name__ == '__main__':
     main()
